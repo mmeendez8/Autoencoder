dc453b7e2697b9ffc1ae38445d807f6752f0adb6
xxxx <miguelmndez@gmail.com>
2019-01-23 13:30:19 +0100

    Latent dimensions plot added

--- autoencoder.py
+++ autoencoder.py
@@ -1,20 +1,25 @@
 import tensorflow as tf
 from tensorflow import keras
+import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import shutil
+import seaborn as sns
+from matplotlib.colors import ListedColormap
 
+sns.set()
 
 # Network parameters
 tf.flags.DEFINE_float('learning_rate', .0005, 'Initial learning rate.')
-tf.flags.DEFINE_integer('epochs', 100, 'Number of steps to run trainer.')
+tf.flags.DEFINE_integer('epochs', 20, 'Number of steps to run trainer.')
 tf.flags.DEFINE_integer('batch_size', 128, 'Minibatch size')
 tf.flags.DEFINE_integer('latent_dim', 10, 'Number of latent dimensions')
 tf.flags.DEFINE_integer('test_image_number', 5, 'Number of test images to recover during training')
 tf.flags.DEFINE_integer('inputs_decoder', 49, 'Size of decoder input layer')
 tf.flags.DEFINE_string('dataset', 'mnist', 'Dataset name [mnist, fashion-mnist]')
 tf.flags.DEFINE_string('logdir', './logs', 'Logs folder')
+tf.flags.DEFINE_bool('plot_latent', True, 'Plot latent space')
 
 FLAGS = tf.flags.FLAGS
 
@@ -83,6 +88,7 @@ def decoder(z, training=False):
         img = tf.reshape(x, shape=[-1, 28, 28, 1])
         return img
 
+
 # Link encoder and decoder
 z, mean_, std_dev = encoder(input_batch)
 output = decoder(z)
@@ -106,34 +112,56 @@ gpu_options = tf.GPUOptions(allow_growth=True)
 # Training loop
 with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
     writer = tf.summary.FileWriter('./logs', sess.graph)
+
     sess.run(init_vars)
     merged_summary_op = tf.summary.merge_all()
 
     for epoch in range(FLAGS.epochs):
         sess.run(iterator.initializer)
-        print(epoch)
-        flag = True  # Show only first image of epoch
+        print('Actual epoch: {}'.format(epoch))
+
+        flag = True  # Show only first batch of epoch
+
         while True:
             try:
                 sess.run(optimizer)
-                if not epoch % 1 and flag:
-                    summ, target, output_, ilos = sess.run([merged_summary_op, input_batch, output, img_loss])
-                    print(ilos)
+                if flag:
+                    # Get input and recover output images comparison
+                    summ, target, output_ = sess.run([merged_summary_op, input_batch, output])
                     f, axarr = plt.subplots(FLAGS.test_image_number, 2)
+
                     for j in range(FLAGS.test_image_number):
                         for pos, im in enumerate([target, output_]):
                             axarr[j, pos].imshow(im[j].reshape((28, 28)), cmap='gray')
                             axarr[j, pos].axis('off')
+
                     plt.savefig(os.path.join(results_folder, 'Train/Epoch_{}').format(epoch))
                     plt.close(f)
                     flag = False
                     writer.add_summary(summ, epoch)
 
-                    # Create random image
+                    # Create artificial image from unit norm
                     artificial_image = sess.run(output, feed_dict={z: np.random.normal(0, 1, (1, FLAGS.latent_dim))})
                     plt.figure()
                     plt.imshow(artificial_image[0].reshape((28, 28)), cmap='gray')
                     plt.savefig(os.path.join(results_folder, 'Test/{}'.format(epoch)))
                     plt.close()
+
+                    # Create plot of latent space (only if latent dimensions are 2)
+                    if FLAGS.latent_dim == 2 and FLAGS.plot_latent:
+                        coords = sess.run(z, feed_dict={input_batch: test_images[..., np.newaxis]/255.})
+                        colormap = ListedColormap(sns.color_palette(sns.hls_palette(10, l=.45 , s=.8)).as_hex())
+                        plt.scatter(coords[:, 0], coords[:, 1], c=test_labels, cmap=colormap)
+
+                        cbar = plt.colorbar()
+                        if FLAGS.dataset == 'fashion-mnist':
+                            cbar.ax.set_yticklabels(['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal',
+                                                     'Shirt', 'Sneaker', 'Bag', 'Ankle boot'])
+
+                        plt.axis('off')
+                        plt.title('Latent space')
+                        plt.savefig(os.path.join(results_folder, 'Test/Latent_{}'.format(epoch)))
+                        plt.close()
+
             except tf.errors.OutOfRangeError:
                 break
